13583b743ac22fe3e0d1332e822d4c1eb42e030d
rbhagat32 <raghavbhagat32@gmail.com>
2025-07-16 19:01:31 +0530

    Update landing page image, text, and spacing for better appearance.

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,14 @@ export default function Home() {
     <div className="poppins">
       <Navbar />
       <CTASection />
-      <div className="w-full flex justify-center items-center mt-10">
+      <div className="w-full flex justify-center items-center mt-14">
         <Image
-          src={"/app.jpg"}
+          src={"/landing.png"}
           alt="dashboard"
-          width={900}
-          height={400}
-          className="shadow-xl aspect-auto sm:w-auto w-[398px] rounded-lg max-w-full   sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl"
+          width={1200}
+          height={900}
+          priority
+          className="shadow-xl aspect-auto sm:w-auto w-[400px] rounded-lg max-w-full sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl"
         />
       </div>
     </div>
@@ -84,23 +85,16 @@ function Buttons() {
 
 function CTASection() {
   return (
-    <div className="flex flex-col mx-16 items-center mt-[120px] gap-6 ">
-      <h2 className="font-bold text-2xl text-center">
+    <div className="flex flex-col mx-16 items-center mt-[100px] gap-6 ">
+      <h2 className="font-bold text-3xl text-center">
         Organize Your Code Snippets
-        <span className={`text-rose-600`}> Efficiently!</span>
+        <span className={`text-rose-600`}> Efficiently !</span>
       </h2>
       <p className="text-center text-sm w-[450px] max-sm:w-full text-slate-500 ">
         With our advanced tagging and search features, you can quickly find the
         snippet you need, right when you need it. Spend less time searching for
         code and more time writing it.
       </p>
-
-      <button
-        className={`block   px-9 py-3 text-sm font-medium text-white transition   focus:outline-none  `}
-        type="button"
-      >
-        {`Let's get started!`}
-      </button>
     </div>
   );
 }
